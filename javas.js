4b5d3e7b2ddefa7198a57c4a8d34de004378d86c
Quicko <quickoabdul@gmail.com>
2022-12-07 19:17:59 +0100

    Js incomplet

--- javas.js
+++ javas.js
@@ -6,13 +6,65 @@
         // Message d'attente
         resultAPI.innerHTML = "Calculs en cours...";
         // Récupération des élèment et suppression des espaces
-        item = document.getElementById("item").value.replace(/\s+/g, "");
-        
         // Définition du endPoint
-        if(item != 0)
+        if(document.getElementById("item"))
         {
-        endPoint = "https://eldenring.fanapis.com/api/items?name=" + encodeURI(item);
+            item = document.getElementById("item").value.replace(/\s+/g, "");
+            if(item != 0)
+            {
+            endPoint = "https://eldenring.fanapis.com/api/items?name=" + encodeURI(item);
+            }
         }
+        
+        if(document.getElementById("ammos"))
+        {
+            ammos = document.getElementById("ammos").value.replace(/\s+/g, "");
+            if(ammos != 0)
+            {
+            endPoint = "https://eldenring.fanapis.com/api/ammos?name=" + encodeURI(ammos);
+            }
+        }
+        if(document.getElementById("armors"))
+        {
+            armors = document.getElementById("armors").value.replace(/\s+/g, "");
+            if(armors != 0)
+            {
+            endPoint = "https://eldenring.fanapis.com/api/armors?name=" + encodeURI(armors);
+            }
+        }
+        if(document.getElementById("bosses"))
+        {
+            bosses = document.getElementById("bosses").value.replace(/\s+/g, "");
+            if(bosses != 0)
+            {
+            endPoint = "https://eldenring.fanapis.com/api/bosses?name=" + encodeURI(bosses);
+            }
+        }
+        if(document.getElementById("creatures"))
+        {
+            creatures = document.getElementById("creatures").value.replace(/\s+/g, "");
+            if(creatures != 0)
+            {
+            endPoint = "https://eldenring.fanapis.com/api/creatures?name=" + encodeURI(creatures);
+            }
+        }
+        if(document.getElementById("locations"))
+        {
+            locations = document.getElementById("locations").value.replace(/\s+/g, "");
+            if(locations != 0)
+            {
+            endPoint = "https://eldenring.fanapis.com/api/locations?name=" + encodeURI(locations);
+            }
+        }
+        if(document.getElementById("weapons"))
+        {
+            weapons = document.getElementById("weapons").value.replace(/\s+/g, "");
+            if(weapons != 0)
+            {
+            endPoint = "https://eldenring.fanapis.com/api/weapons?name=" + encodeURI(weapons);
+            }
+        }
+        
 
         // Appel AJAX de l’API
         ajaxAPI(endPoint, "GET", function (reponse) 
@@ -38,7 +90,8 @@
         contenu = '<ul>';
         for (let i = 0 ; i < Object.keys(objet).length ; i++ ) 
         {
-            if ( Object.values(objet)[i] !== null ) {
+            if ( Object.values(objet)[i] !== null ) 
+            {
                 contenu += '<li>' + Object.keys(objet)[i] + ' : ';
                 if ( typeof Object.values(objet)[i] === 'object' ) 
                 {
@@ -49,8 +102,9 @@
                     contenu += Object.values(objet)[i];
                 }
                 contenu += '</li>';
-                }
-                }
+            }
+        }
                 contenu += '</ul>';
                 return contenu;
     }
+// Il faut réparer cette fonction la en s'inspirant des autres, 
